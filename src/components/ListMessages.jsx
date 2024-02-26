2d9281626c22700fd5e9ddcaca48b3a8cb7dae26
Trương Dương Minh Nhật <jimtruong13092003@gmail.com>
2024-02-26 07:42:06 +0700

    Done load message for the group's chat

--- src/components/ListMessages.jsx
+++ src/components/ListMessages.jsx
@@ -21,21 +21,21 @@ const listMessages = ({ data }) => {
                     personName: "Trương Dương Minh Nhật",
                     content: "OK. Cách này cũng dc nhưng mà nó rườm rà, dài dòng quá, không có cách nào tối ưu hơn hay sao mà phải dùng đến cách này vậy?",
                     time: "16:51",
-                    image: ""
+                    image: "https://picsum.photos/720/348"
                 },
                 {
                     personID: 1,
                     personName: "Nguyễn Huy Hoàng",
                     content: "Thử làm cách này xem, cách này ổn ko làm, đi làm cách khác?",
                     time: "16:50",
-                    image: ""
+                    image: "https://picsum.photos/640/480"
                 },
                 {
                     personID: 1,
                     personName: "Nguyễn Huy Hoàng",
                     content: "Dr, làm theo cách đó thôi chứ còn cách nào khác đâu?",
                     time: "16:50",
-                    image: ""
+                    image: "https://picsum.photos/640/480"
                 }
             ]
         },
@@ -44,25 +44,40 @@ const listMessages = ({ data }) => {
             primaryPersonID: 1,
             history_messages: [
                 {
+                    personID: 3,
+                    personName: "Trần Văn Tú",
+                    content: "OK. Có gì cho tui xin tạm 3 chương đầu vậy",
+                    time: "12:03",
+                    image: "https://picsum.photos/640/350",
+
+                },
+                {
+                    personID: 1,
+                    personName: "Nguyễn Huy Hoàng",
+                    content: "Tui làm xong được chương 3 chương đầu, còn mấy chương kia tui chưa có làm",
+                    time: "12:02",
+                    image: "https://picsum.photos/640/480",
+                },
+                {
                     personID: 2,
                     personName: "Trương Dương Minh Nhật",
-                    content: "OK. Cách này cũng dc nhưng mà nó rườm rà, dài dòng quá, không có cách nào tối ưu hơn hay sao mà phải dùng đến cách này vậy?",
-                    time: "16:51",
-                    image: ""
+                    content: "Chưa có làm xong, từ từ đi. Mới giao mà làm gì có ai mà làm xong hết các chương vậy.",
+                    time: "12:01",
+                    image: "https://picsum.photos/720/348",
                 },
                 {
-                    personID: 1,
-                    personName: "Nguyễn Huy Hoàng",
-                    content: "Thử làm cách này xem, cách này ổn ko làm, đi làm cách khác?",
-                    time: "16:50",
-                    image: ""
+                    personID: 3,
+                    personName: "Trần Văn Tú",
+                    content: "Cho tui xin trước chương 1 đến chương 5 đi, còn các chương khác thì từ từ.",
+                    time: "12:01",
+                    image: "https://picsum.photos/640/350"
                 },
                 {
-                    personID: 1,
-                    personName: "Nguyễn Huy Hoàng",
-                    content: "Dr, làm theo cách đó thôi chứ còn cách nào khác đâu?",
-                    time: "16:50",
-                    image: ""
+                    personID: 3,
+                    personName: "Trần Văn Tú",
+                    content: "Alo? Mọi người làm xong hết toàn bộ các chương chưa dợ? Cho tui xin vs.",
+                    time: "12:00",
+                    image: "https://picsum.photos/640/350"
                 }
             ]
         }
