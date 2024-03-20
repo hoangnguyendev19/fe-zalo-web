05ea3cf4dfb80f7516370b47457c8037b5d7ee15
Dang Quang <dangquang@duck.com>
2024-03-20 22:59:30 +0700

    Complete sidebar UI admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -8,12 +8,25 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TextField,
+  Button,
 } from "@mui/material";
-import React, { useState } from 'react';
+// import { DataGrid } from '@mui/x-data-grid';
+import React, { useState } from "react";
 
 import PersonIcon from "@mui/icons-material/Person";
+import SearchIcon from "@mui/icons-material/Search";
+import LogoutIcon from '@mui/icons-material/Logout';
+
 const Admin = () => {
-  const [selectedContent, setSelectedContent] = useState('userManagement');
+  const [selectedContent, setSelectedContent] = useState("Quản lí người dùng");
   return (
     <Box
       sx={{
@@ -22,17 +35,17 @@ const Admin = () => {
       }}
     >
       <Grid container sx={{ height: "100vh" }}>
-        <Grid item xs={2.25} sx={{ borderRight: "1px solid rgba(0,0,0,0.3)" }}>
-          <SideBar setSelectedContent={setSelectedContent}/>
+        <Grid item sx={{ borderRight: "1px solid rgba(0,0,0,0.3)", width: '230px' }}>
+          <SideBar setSelectedContent={setSelectedContent} />
         </Grid>
-        <Grid item xs={9.75} sx={{ textAlign: "center" }}>
-          <MainContent selectedContent={selectedContent}/>
+        <Grid item xs sx={{ textAlign: "center" }}>
+          <MainContent selectedContent={selectedContent} />
         </Grid>
       </Grid>
     </Box>
   );
 };
-const SideBar = ({setSelectedContent}) => {
+const SideBar = ({ setSelectedContent }) => {
   return (
     <Box
       sx={{
@@ -40,6 +53,7 @@ const SideBar = ({setSelectedContent}) => {
         height: "100vh",
         paddingTop: "10px",
         boxSizing: "border-box",
+        backgroundColor: "#2F323B",
       }}
     >
       <Box
@@ -66,14 +80,13 @@ const SideBar = ({setSelectedContent}) => {
             src="/static/images/avatar/1.jpg"
             sx={{ width: 80, height: 80, marginBottom: "20px" }}
           />
-          <Typography>
+          <Typography sx={{color: 'white'}}>
             <strong>Admin</strong>
           </Typography>
         </Box>
         {/* List button */}
         <List sx={{ flexGrow: 1 }}>
-          <UserManagement setSelectedContent={setSelectedContent}/>
-
+          <Tab setSelectedContent={setSelectedContent} Title={"Quản lí người dùng"}/>
         </List>
         {/* Button sign out */}
         <Box
@@ -84,45 +97,62 @@ const SideBar = ({setSelectedContent}) => {
             marginTop: "20px",
           }}
         >
-          <button>Sign out</button>
+          <Button
+            startIcon={<LogoutIcon/>}
+            sx={{
+              padding: "10px 20px",
+              backgroundColor: "#546379",
+              color: "white",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+              width: "150px",
+              marginBottom: "15px",
+              '&:hover': {
+                backgroundColor: '#7091BC', // Replace 'yourColor' with the color you want
+              },
+            }}
+          >
+            Logout
+          </Button>
         </Box>
       </Box>
     </Box>
   );
 };
 // Content of Admin page
-const MainContent = ({selectedContent}) => {
-  
+const MainContent = ({ selectedContent }) => {
   return (
     <Box sx={{ width: "100%", marginTop: "10px" }}>
-      {selectedContent === 'userManagement' && <UserList />}
-      
+      <Typography>Hello</Typography>
     </Box>
   );
 };
 // User management
-const UserManagement = ({ setSelectedContent }) => {
+const Tab = ({ setSelectedContent, Title }) => {
   const handleClick = () => {
-    setSelectedContent('userManagement');
+    setSelectedContent(Title);
   };
 
   return (
     <ListItem disablePadding>
-      <ListItemButton onClick={handleClick}>
-        <ListItemIcon sx={{ minWidth: 35 }}>
+      <ListItemButton onClick={handleClick} sx={{
+          backgroundColor: '#3598DB',
+          borderRadius: '5px',
+          margin: '10px 5px',
+          '&:hover': {
+            backgroundColor: '#47D2EF', // Replace 'yourColor' with the color you want
+          },  
+        
+      }}>
+        <ListItemIcon sx={{ minWidth: 35, color: 'white' }}>
           <PersonIcon />
         </ListItemIcon>
-        <ListItemText primary="Quản lí người dùng" />
+        <ListItemText primary={Title} sx={{ color: 'white' }} />
       </ListItemButton>
     </ListItem>
   );
 };
-const UserList = () => {
-  return (
-    <Box sx={{ padding: "10px 20px" }}>
-      <Typography>Không có tin nhắn chưa đọc</Typography>
-    </Box>
-  );
-};
+
 
 export default Admin;
